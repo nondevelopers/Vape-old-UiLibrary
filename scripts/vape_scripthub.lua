197ffd25c6ec7eed2d538d22bccc621d7fa5d23b
nondevelopers <mozaireal@gmail.com>
2026-07-17 12:17:55 -0400

    Update and rename vape_protector.lua to vape_scripthub.lua

--- scripts/vape_scripthub.lua
+++ scripts/vape_scripthub.lua
@@ -1,7 +1,7 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/nondevelopers/Vape-UiLibrary/main/source.lua"))()
-local win = lib:Window("Vape Protection", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+local win = lib:Window("Vape Scripthub", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
 -tabs
-local tab = win:Tab("anti-")
+local tab = win:Tab("Main")
 
 tab:Button("Button", function()
     
