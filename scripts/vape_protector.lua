48aa3e8b2be25b6387db659d81a8a8a665eebf87
nondevelopers <mozaireal@gmail.com>
2026-07-17 12:08:17 -0400

    Create vape_protector.lua

---
+++ scripts/vape_protector.lua
@@ -0,0 +1,8 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/nondevelopers/Vape-UiLibrary/main/source.lua"))()
+local win = lib:Window("Vape Protection", Color3.fromRGB(44, 120, 224), Enum.KeyCode.RightControl)
+-tabs
+local tab = win:Tab("anti-")
+
+tab:Button("Button", function()
+    
+end)
